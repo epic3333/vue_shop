c45565343feb1c6a2b2c2687c81718f12ccbbc6b
epic3333 <epic33@mail.ru>
2023-06-09 10:02:33 +0300

    feat: connect router in project

---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+const router = new VueRouter();
+export default router;
