0df6618023bdb70b607e96ec7dc04a3293f094fe
epic3333 <epic33@mail.ru>
2023-06-09 10:05:37 +0300

    feat: add routesArr, connect routes in Vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,5 +3,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
-const router = new VueRouter();
+const routes = [];
+const router = new VueRouter({
+  routes,
+});
 export default router;
