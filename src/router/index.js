984df7e42d3676fc4dc942c2602d1206e29f261c
epic3333 <epic33@mail.ru>
2023-06-09 10:09:25 +0300

    feat: add MainPage in routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import VueRouter from 'vue-router';
+import MainPage from '../pages/MainPage.vue';
 
 Vue.use(VueRouter);
-const routes = [];
+const routes = [
+  { name: 'main', component: MainPage, path: '/' },
+];
 const router = new VueRouter({
   routes,
 });
