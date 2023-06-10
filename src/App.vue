78446197c1d0c41a2e8686b7e9684802e9ac5785
epic3333 <epic33@mail.ru>
2023-06-10 22:15:17 +0300

    feat: add header, footer component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <router-view />
+  <div>
+    <HeaderMain></HeaderMain>
+    <router-view />
+    <FooterMain></FooterMain>
+  </div>
 </template>
 
 <script>
+import HeaderMain from './components/HeaderMain.vue';
+import FooterMain from './components/FooterMain.vue';
 
 export default {
+  name: 'App',
+  components: { HeaderMain, FooterMain },
 };
 </script>
