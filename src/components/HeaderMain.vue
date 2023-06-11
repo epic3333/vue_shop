6c0a4081d7f856ed933d92a4ef313d209cc415ec
epic3333 <epic33@mail.ru>
2023-06-11 14:33:06 +0300

    feat: add link to MainPage

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="header">
       <div class="header__wrapper container">
-        <span class="header__info">Каталог</span>
+        <router-link class="header__info" :to="{name: 'main'}">Каталог</router-link>
 
         <a class="header__logo" href="#">
           <img src="img/svg/logo-tech.svg" alt="Логотип интернет магазина Технозавррр" width="190" height="33">
