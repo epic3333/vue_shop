740b12bbd624338f34e463f1c27674f032521d1c
epic3333 <epic33@mail.ru>
2023-06-13 18:12:22 +0300

    feat: add component CartItem

---
+++ src/components/CartItem.vue
@@ -0,0 +1,63 @@
+<template>
+    <li class="cart__item product">
+        <div class="product__pic">
+        <img :src="item.product.image" width="120" height="120"
+                :alt="item.product.title">
+        </div>
+        <h3 class="product__title">
+        {{ item.product.title }}
+        </h3>
+        <p class="product__info product__info--color">
+        Цвет:
+        <span>
+            <i :style="'background-color:' + item.product.colorVal"></i>
+            {{ item.product.colorName }}
+        </span>
+        </p>
+        <span class="product__code">
+        Артикул: {{ item.productId }}
+        </span>
+
+        <div class="product__counter form__counter">
+        <button type="button" aria-label="Убрать один товар">
+            <svg width="10" height="10" fill="currentColor">
+            <use xlink:href="#icon-minus"></use>
+            </svg>
+        </button>
+
+        <input type="text" :value="item.amount" name="count">
+
+        <button type="button" aria-label="Добавить один товар">
+            <svg width="10" height="10" fill="currentColor">
+            <use xlink:href="#icon-plus"></use>
+            </svg>
+        </button>
+        </div>
+
+        <b class="product__price">
+        {{ (item.amount * item.product.price) | numberFormat }} ₽
+        </b>
+
+        <button class="product__del button-del" type="button" aria-label="Удалить товар из корзины">
+        <svg width="20" height="20" fill="currentColor">
+            <use xlink:href="#icon-close"></use>
+        </svg>
+        </button>
+    </li>
+</template>
+
+<script>
+import numberFormat from '../helpers/numberFormat';
+
+export default {
+  name: 'CartItem',
+  props: ['item'],
+  filters: {
+    numberFormat,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
